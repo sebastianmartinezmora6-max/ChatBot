e0b40872a5c5829118a95226ec20e11bf4a5d808
sebastianmartinezmora6-max <sebastianmartinezmora6@gmail.com>
2026-01-05 11:08:32 -0500

    Create Bot.py
    Un bot Responde solo 3 comandos

---
+++ Bot.py
@@ -0,0 +1,27 @@
+import discord
+import random 
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices)) 
+
+bot.run("Token")
